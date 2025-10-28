3edde75cb70d61abfa110f8738f7d7c52f312764
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-10-28 13:25:11 +0900

    Bronze5 10869

---
+++ solved/Bronze/10869.py
@@ -0,0 +1,8 @@
+arr = list(map(int, input().split()))
+
+# 몫만 나오게하는 산술연산자는 // 이거임. C++이랑 다름.
+print(arr[0]+arr[1])
+print(arr[0]-arr[1])
+print(arr[0]*arr[1])
+print(arr[0]//arr[1])
+print(arr[0]%arr[1])
