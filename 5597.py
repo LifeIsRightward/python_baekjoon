06330afbcfda7e7bec8123a46904d9881b4b82e7
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-10-29 00:41:33 +0900

    unsolved

--- 5597.py
+++ 5597.py
@@ -1,6 +1,21 @@
 import sys
 
-arr = []
+# 30까지 False로 init
+# 방법1
+arr = [False] * 30
 
+# 30까지 False로 init
+# 방법2
+'''
+for x in range(30) :
+    arr.append(False)
+'''
+
+# 과제 제출 28명 True
 for i in range(28) :
-    arr.append(int(input().split()))
+    arr[int(input())] = True
+
+for y in range(30) :
+    if arr[y] != True :
+        print(arr[y]) 
+
