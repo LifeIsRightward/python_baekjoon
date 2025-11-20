cbef2b929df662beffefceebf52d104753d41315
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-11-21 00:35:15 +0900

    not solved 20291

---
+++ 20291.py
@@ -0,0 +1,20 @@
+import sys
+
+# 파일 개수 N
+n = int(sys.stdin.readline().rstrip())
+
+# {} -> 이건 딕셔너리임.
+extension_name = {}
+
+
+for _ in range(n) :
+    name, extension = sys.stdin.readline().rstrip().split()
+    
+    if extension_name.count() == 0 :
+        extension_name.append((extension, 1))
+    else :
+        extension_name.
+    
+
+
+    
