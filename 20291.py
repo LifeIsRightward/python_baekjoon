a3c74fc9492a2c8cb2f2facf1ba0dc6dcbee2cd5
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-11-24 10:14:00 +0900

    Dictionary

--- 20291.py
+++ 20291.py
@@ -8,13 +8,18 @@ extension_name = {}
 
 
 for _ in range(n) :
-    name, extension = sys.stdin.readline().rstrip().split()
+    name, extension = sys.stdin.readline().rstrip().split(".")
     
-    if extension_name.count() == 0 :
-        extension_name.append((extension, 1))
+    if extension not in extension_name :
+        extension_name[extension] = 1
     else :
-        extension_name.
+        extension_name[extension] += 1
     
 
+# 딕셔너리를 sorting 하면 튜플로 이루어진 list를 반환한다.
+# key: value에서 Key를 기준으로 오름차순 정렬
+sorted_dict = dict(sorted(extension_name.items()))
 
-    
+# 내가 딕셔너리 구조를 잘 몰라서... 공부해야할 듯
+for key, value in sorted_dict.items() :
+    print(f'{key} {value}')  
