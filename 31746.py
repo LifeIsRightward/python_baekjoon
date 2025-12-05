cbc5c749b60225f443c8e52b47d120d16e28554c
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-12-06 01:09:15 +0900

    Basic

---
+++ 31746.py
@@ -0,0 +1,11 @@
+import sys
+
+fucked_up1 = "evoLmoCicS"
+fucked_up2 = "SciComLove"
+
+n = int(sys.stdin.readline().rstrip())
+
+if n % 2 == 0 :
+    print(fucked_up2)
+else :
+    print(fucked_up1)
