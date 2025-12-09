17e0ddf85a17993a71e25963bcc15aade9bc4c87
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-12-10 00:39:04 +0900

    Basic

---
+++ solved/Bronze/34813.py
@@ -0,0 +1,12 @@
+import sys
+
+aaa = sys.stdin.readline().rstrip()
+
+if aaa[0] == 'F' : 
+    print("Foundation")
+elif aaa[0] == 'C' :
+    print("Claves")
+elif aaa[0] == 'V' :
+    print("Veritas")
+elif aaa[0] == 'E' :
+    print("Exploration")
